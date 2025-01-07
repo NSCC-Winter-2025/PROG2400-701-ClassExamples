e90801d499fbfe5f001c476ccd2a2d5afe5612f8
Brian Shewan <brian.shewan@nscc.ca>
2025-01-07 13:38:56 -0400

    added echo

---
+++ echo/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string_view>
+
+using namespace std::literals;
+
+int main()
+{
+    constexpr auto END = "end"sv;
+
+    const auto get_input = []()
+    {
+        std::string input;
+        std::cout << "Enter a line of text: "sv;
+        std::getline(std::cin, input);
+        return input;
+    };
+
+    std::string user_input;
+    while ((user_input = get_input()) != END)
+    {
+        std::cout << R"(You typed: ")"sv << user_input << R"(")"sv << std::endl;
+    }
+}
+
